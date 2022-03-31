f609edc74769e35d3b00f9e847af901320b067bd
b123b123 <02684200@163.com>
2022-03-31 17:35:10 +0800

    Vue测试项目

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    {{message | test("nihao")}}
+    <br>
+    <br>
+    <button @click="change">点击 +</button>
+    学习git实验中
   </div>
 </template>
+
+<script>
+let wrap = {
+  message:2
+}
+export default {
+  data(){
+    return wrap;
+  },
+  methods:{
+    change(){
+      this.message ++
+    }
+  },
+  filters:{
+    test(val,val2){
+      console.log(val);
+      console.log(val2);
+      return val;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
