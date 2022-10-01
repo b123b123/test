de250bd8a0e25a89cf450b004ae21225162db3e5
b123b123 <02684200@163.com>
2022-10-01 10:51:34 +0800

    优化

--- src/views/test20.vue
+++ src/views/test20.vue
@@ -1,51 +1,15 @@
 <!--
- * @Description: 
- * @Version: 2.0
- * @Author: wuyue.nan
- * @Date: 2022-07-06 14:00:34
- * @LastEditors: wuyue.nan
- * @LastEditTime: 2022-07-06 14:12:19
+ * @Description: Hello World
+ * @Version: 1.0
+ * @Author: WangBo
+ * @Date: 2022-10-01 10:50:15
+ * @LastEditors: WangBo
+ * @LastEditTime: 2022-10-01 10:51:22
 -->
-<template>
-  <div>
-    <div v-if="test" class="box">小于1366</div>
-    <div v-if="!test" class="box">大于了1366</div>
-  </div>
-</template>
+<template></template>
 
 <script>
-export default {
-  data() {
-    return {
-      test: true,
-    };
-  },
-  mounted() {
-    this.getWidth();
-  },
-  methods: {
-    getWidth() {
-      let innerWidth = window.innerWidth;
-      console.log("innerWidth ==> ", innerWidth);
-      if (innerWidth > 1366) {
-        this.test = false;
-      } else {
-        this.test = true;
-      }
-    },
-  },
-};
+export default {};
 </script>
 
-<style scoped>
-.box {
-  width: 100vw;
-  height: 100vh;
-  background: aqua;
-}
-@media screen and (min-width: 1366px) {
-  .box {
-    background: salmon;
-  }
-}
-</style>
+<style></style>
