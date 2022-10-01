d2a784d7423a9cdca9d3aaf3198b909971ba9dc3
b123b123 <02684200@163.com>
2022-10-01 11:28:51 +0800

    优化

--- src/views/test20.vue
+++ src/views/test20.vue
@@ -4,20 +4,48 @@
  * @Author: WangBo
  * @Date: 2022-10-01 10:50:15
  * @LastEditors: WangBo
- * @LastEditTime: 2022-10-01 10:53:32
+ * @LastEditTime: 2022-10-01 11:23:38
 -->
 <template>
-  <div>HelloWorld</div>
+  <div>
+    <div class="box" @click="test.ceshi('nihaoasasdas'), add">HelloWorld!</div>
+    <div class="test">{{ test }}</div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
+    let that = this;
+    let test = {
       test: 1,
+      nihao: this.add,
+      ceshi(a) {
+        console.log("this ==> ", this.test);
+        console.log("that ==> ", that);
+        console.log("a ==> ", a);
+      },
     };
+    test.ceshi("nihao");
+    return {
+      test,
+    };
+  },
+  methods: {
+    add() {
+      this.test.test++;
+    },
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+.test {
+  height: 100px;
+  background: aquamarine;
+}
+.box {
+  height: 30px;
+  background: wheat;
+}
+</style>
