0b85ff7d3314bcca23ac25557c6f3e14b12e989d
b123b123 <02684200@163.com>
2022-11-09 13:44:25 +0800

    测试

--- src/views/test23.vue
+++ src/views/test23.vue
@@ -4,12 +4,28 @@
  * @Author: WangBo
  * @Date: 2022-11-09 10:39:39
  * @LastEditors: WangBo
- * @LastEditTime: 2022-11-09 11:05:50
+ * @LastEditTime: 2022-11-09 13:39:52
 -->
 <template>
   <div>测试</div>
 </template>
 
 <script>
-export default {};
+export default {
+  async mounted() {
+    let test = true;
+    console.log(1);
+    if (test) {
+      console.log(2);
+      // import("../utils/index1").then(({ hello: test }) => {
+      //   console.log(test);
+      //   console.log(5);
+      // });
+      let test = await import("../utils/index1");
+      console.log(test);
+      console.log(3);
+    }
+    console.log(4);
+  },
+};
 </script>
