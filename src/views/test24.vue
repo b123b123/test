314becb37de25e879e7f89405445418d9c789877
b123b123 <02684200@163.com>
2022-11-09 14:23:29 +0800

    优化

--- src/views/test24.vue
+++ src/views/test24.vue
@@ -4,7 +4,7 @@
  * @Author: WangBo
  * @Date: 2022-11-09 10:39:39
  * @LastEditors: WangBo
- * @LastEditTime: 2022-11-09 13:52:11
+ * @LastEditTime: 2022-11-09 14:23:08
 -->
 <template>
   <div>测试</div>
