fb59cd72428ce106e47fc9e84418d2b4dd9835ac
b123b123 <02684200@163.com>
2022-11-09 13:51:53 +0800

    动态加载模块测试

--- src/views/test23.vue
+++ src/views/test23.vue
@@ -4,7 +4,7 @@
  * @Author: WangBo
  * @Date: 2022-11-09 10:39:39
  * @LastEditors: WangBo
- * @LastEditTime: 2022-11-09 13:39:52
+ * @LastEditTime: 2022-11-09 13:49:41
 -->
 <template>
   <div>测试</div>
@@ -17,12 +17,15 @@ export default {
     console.log(1);
     if (test) {
       console.log(2);
+      let str = "utils/index1";
+      console.log(str);
       // import("../utils/index1").then(({ hello: test }) => {
-      //   console.log(test);
-      //   console.log(5);
-      // });
-      let test = await import("../utils/index1");
-      console.log(test);
+      import(`../${str}`).then(({ hello: test }) => {
+        console.log(test);
+        console.log(5);
+      });
+      // let test = await import("../utils/index1");
+      // console.log(test);
       console.log(3);
     }
     console.log(4);
